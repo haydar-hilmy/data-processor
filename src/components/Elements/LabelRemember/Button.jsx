12ebd01a254d47ec35912b36048542ce92808332
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-11-14 09:46:59 +0700

    rm incorrect components folder (lowercase)

--- src/components/Elements/LabelRemember/Button.jsx
+++
@@ -1,9 +0,0 @@
-const ButtonLabelRemember = (props) => {
-    const { text, variant, onclick, oncontextmenu, ontouchStart, ontouchEnd } = props
-    return (
-        <>
-            <button onTouchStart={ontouchStart} onTouchEnd={ontouchEnd} onContextMenu={oncontextmenu} onClick={onclick} className={`${variant != null ? variant : ""} bg-gray-700 px-5 py-2 rounded-md text-primary-dark-0 duration-150 hover:opacity-60 cursor-pointer select-none`}>{text}</button>
-        </>
-    )
-}
-export default ButtonLabelRemember

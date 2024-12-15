0e4f96b2a6a8bf2fd11a2448c811144265fb9856
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2024-12-15 18:27:38 +0700

    fix: Tensorflow Clf NN (numeric ouput)

--- src/Function/TensorFlow/ProcessModel.jsx
+++ src/Function/TensorFlow/ProcessModel.jsx
@@ -10,7 +10,7 @@ import { getSubset, normalizeDataset, valueOfColumn } from "./DataManipulate";
 
 function NeuralNetwork(
     trainingData = [{}, {}],
-    testingData = [{"feat1": "val1", "feat2": "val2"}],
+    testingData = [{ "feat1": "val1", "feat2": "val2" }],
     label = "",
     features = [""],
     option = {
@@ -21,58 +21,65 @@ function NeuralNetwork(
     let selectedLabelFeat = [...features] // format = ["", ""]
     let LetTrainingData = [...trainingData] // format = [{"feat1": "val1"}, {"feat2": "val2"}] || all fitur (include label)
     let LetTestingData = [...testingData] // format = [{"feat1": "val1", "feat2": "val2"}]
+    // console.group("INITIALIZE VARIABLE")
+    // console.info("SelectedlabelFeat: ", selectedLabelFeat)
+    // console.info("LetTrainingData: ", LetTrainingData)
+    // console.info("LetTestingData: ", LetTestingData)
+    // console.groupEnd()
 
-    console.group("INITIALIZE VARIABLE")
-    console.info("SelectedlabelFeat: ", selectedLabelFeat)
-    console.info("LetTrainingData: ", LetTrainingData)
-    console.info("LetTestingData: ", LetTestingData)
-    console.groupEnd()
-    
-    
+
     // SUBSET DATA
     LetTrainingData = getSubset(LetTrainingData, option.subSet)
-    console.group("SUBSET DATA")
-    console.info("LetTrainingData: ", LetTrainingData)
-    console.groupEnd()
+    // console.group("SUBSET DATA")
+    // console.info("LetTrainingData: ", LetTrainingData)
+    // console.groupEnd()
+
+    // GET LABEL DATA
+    let selectedLabelData = sliceObj([...LetTrainingData], [label])
+    // console.group("GET LABEL AND NORMALIZE")
+    // console.info("selectedLabelData: ", selectedLabelData)
+    // selectedLabelData = transformArray(normalizeDataset(selectedLabelData))
+    // console.info("selectedLabelData normalize: ", selectedLabelData)
+    // console.groupEnd()
 
     // SLICING ONLY FEATURE
     let selectedTrainingData = sliceObj(LetTrainingData, selectedLabelFeat) // exclude label
-    console.group("SLICING ONLY FEATURE")
-    console.info("selectedTrainingData: ", selectedTrainingData)
-    console.groupEnd()
+    // console.group("SLICING ONLY FEATURE")
+    // console.info("selectedTrainingData: ", selectedTrainingData)
+    // console.groupEnd()
 
     // TESTING DATA JOIN TRAINING DATA TO NORMALIZE TOGETHER
     selectedTrainingData = [...selectedTrainingData, ...LetTestingData]
-    console.group("TESTING DATA JOIN TRAINING DATA TO NORMALIZE TOGETHER")
-    console.info("selectedTrainingData: ", selectedTrainingData)
-    console.info("LetTestingData: ", LetTestingData)
-    console.groupEnd()
-    
+    // console.group("TESTING DATA JOIN TRAINING DATA TO NORMALIZE TOGETHER")
+    // console.info("selectedTrainingData: ", selectedTrainingData)
+    // console.info("LetTestingData: ", LetTestingData)
+    // console.groupEnd()
+
+    // NORMALIZE TOGETHER
     selectedTrainingData = transformArray(normalizeDataset(selectedTrainingData))
-    console.group("NORMALIZE TOGETHER")
-    console.info("selectedTrainingData: ", selectedTrainingData)
-    console.groupEnd()
+    // console.group("NORMALIZE TOGETHER")
+    // console.info("selectedTrainingData: ", selectedTrainingData)
+    // console.groupEnd()
 
     // TAKE BACK TESTING DATA FROM TRAINING DATA
     let selectedTestingData = selectedTrainingData.slice(-1)[0] // format = [[0, 2]]
-    console.group("TAKE BACK TESTING DATA FROM TRAINING DATA")
-    console.info("selectedTestingData: ", selectedTestingData)
-    console.groupEnd()
+    // console.group("TAKE BACK TESTING DATA FROM TRAINING DATA")
+    // console.info("selectedTestingData: ", selectedTestingData)
+    // console.groupEnd()
 
     // // DROP TESTING DATA FROM TRAINING DATA
     selectedTrainingData.pop() // format = [[0, 1, 2, 4]...]
-    console.group("DROP TESTING DATA FROM TRAINING DATA")
-    console.info("selectedTrainingData: ", selectedTrainingData)
-    console.groupEnd()
+    // console.group("DROP TESTING DATA FROM TRAINING DATA")
+    // console.info("selectedTrainingData: ", selectedTrainingData)
+    // console.groupEnd()
 
-    // let selectedLabelData = sliceObj(LetTrainingData, [label]) // only label
-    // selectedLabelData = transformArray(normalizeDataset(selectedLabelData))
+
 
 
     // TENSORFLOW
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 16, inputShape: [4], activation: 'relu' }));
-    model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));  // 1 unit untuk 2 kelas (0 atau 1)
+    model.add(tf.layers.dense({ units: 16, inputShape: [selectedTrainingData[0].length], activation: 'relu' }));
+    model.add(tf.layers.dense({ units: selectedLabelData[0].length, activation: 'sigmoid' }));  // units is length of Label Column
 
     model.compile({
         optimizer: 'adam',
@@ -80,19 +87,25 @@ function NeuralNetwork(
         metrics: ['accuracy']
     });
 
-    // const xs = tf.tensor2d(selectedTrainingData); // only Feature
+    const xs = tf.tensor2d(selectedTrainingData)
+    const ys = tf.tensor2d(selectedLabelData)
 
-    // const ys = tf.tensor2d(selectedLabelData); // Label biner 0 atau 1
+    // console.group("INPUT XS YS")
+    // console.info("XS: selectedTrainingData: ", selectedTrainingData)
+    // console.info("Length: ", [selectedTrainingData.length, selectedTrainingData[0].length])
+    // console.info("YS: selectedLabelData: ", selectedLabelData)
+    // console.info("Length: ", [selectedLabelData.length, selectedLabelData[0].length])
+    // console.groupEnd()
 
-    // model.fit(xs, ys, { epochs: 10 }).then(() => {
-    //     // Prediksi untuk input baru
-    //     model.predict(tf.tensor2d([TestingData], [1, 3])).print();
-    // });
+    model.fit(xs, ys, { epochs: 10 }).then(() => {
+        // Prediksi untuk input baru
+        model.predict(
+            tf.tensor2d([selectedTestingData], [1, selectedTrainingData[0].length])
+        ).print();
+    });
+
 }
 
 
 
 export { NeuralNetwork }
-
-// MEMBUAT KONFIGURASI LABEL
-// PERSIAPAN DATA FEATURE DAN TESTING SUDAH SIAP (INCLUDE NORMALIZE)
